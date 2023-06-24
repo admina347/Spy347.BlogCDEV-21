fea4a73b06f49186eb43f45d747be392ed20d836
admina <it-mactep@mail.ru>
2023-06-24 13:39:29 +0500

    add DI

--- Spy347.BlogCDEV-21.API/Program.cs
+++ Spy347.BlogCDEV-21.API/Program.cs
@@ -1,11 +1,69 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Spy347.BlogCDEV_21.Infrastructure;
+using Spy347.BlogCDEV_21.Infrastructure.Models;
+using Spy347.BlogCDEV_21.Infrastructure.Repositories;
+using Spy347.BlogCDEV_21.Web.BLL;
+using Spy347.BlogCDEV_21.Web.BLL.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//БД
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Identity password settings
+builder.Services.AddIdentity<User, Role>(opts =>
+{
+    opts.Password.RequiredLength = 5;
+    opts.Password.RequireNonAlphanumeric = false;
+    opts.Password.RequireLowercase = false;
+    opts.Password.RequireUppercase = false;
+    opts.Password.RequireDigit = false;
+})
+  .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// регистрация сервиса репозитория для взаимодействия с базой данных  
+builder.Services
+  .AddTransient<ICommentRepository, CommentRepository>()
+  .AddTransient<ITagRepository, TagRepository>()
+  .AddTransient<IPostRepository, PostRepository>()
+  .AddTransient<IAccountService, AccountService>()
+  .AddTransient<ICommentService, CommentService>()
+  .AddTransient<IHomeService, HomeService>()
+  .AddTransient<IPostService, PostService>()
+  .AddTransient<ITagService, TagService>()
+  .AddTransient<IRoleService, RoleService>();
+
+// Подключаем автомаппинг
+var mapperConfig = new MapperConfiguration((v) =>
+{
+    v.AddProfile(new MappingProfile());
+}
+);
+
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+builder.Services.AddAuthentication(optionts => optionts.DefaultScheme = "Cookies")
+  .AddCookie("Cookies", options =>
+  {
+    options.Events = new Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationEvents
+    {
+      OnRedirectToLogin = redirectContext =>
+      {
+        redirectContext.HttpContext.Response.StatusCode = 401;
+        return Task.CompletedTask;
+      }
+    };
+  });
+
 
 var app = builder.Build();
 
